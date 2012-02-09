ae51cc8d2aaf0fb682e795b10a35391b48fb216e
Enrique García Cota <kikito@gmail.com>
2012-02-09 11:37:16 +0100

    changed variable names

--- app/models/project_join_request_mailer.rb
+++ app/models/project_join_request_mailer.rb
@@ -3,7 +3,7 @@ class ProjectJoinRequestMailer < Mailer
   def join_request(project_join_request)
 
     recipients project_join_request.approvers.collect(&:mail)
-    subject "[#{project_join_request.project.name}] #{l(:join_project_text_request_to_join)}"
+    subject "[#{project_join_request.project.name}] #{l(:join_project_text_request_to_join_subject)}"
 
     body({:project_join_request => project_join_request})
     render_multipart('join_request', body)
@@ -11,7 +11,7 @@ class ProjectJoinRequestMailer < Mailer
 
   def declined_request(project_join_request)
     recipients project_join_request.user.mail
-    subject "[#{project_join_request.project.name}] #{l(:join_project_text_declined_request_to_join_this_project)}"
+    subject "[#{project_join_request.project.name}] #{l(:join_project_text_declined_request_to_join_this_project_subject)}"
 
     body({:project_join_request => project_join_request})
     render_multipart('declined_request', body)
@@ -19,7 +19,7 @@ class ProjectJoinRequestMailer < Mailer
 
   def accepted_request(project_join_request)
     recipients project_join_request.user.mail
-    subject "[#{project_join_request.project.name}] #{l(:join_project_text_accepted_request_to_join_this_project)}"
+    subject "[#{project_join_request.project.name}] #{l(:join_project_text_accepted_request_to_join_this_project_subject)}"
 
     body({:project_join_request => project_join_request})
     render_multipart('accepted_request', body)
